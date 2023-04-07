04e9fa9e3ccb286cbbe131c4b21faa8af408c291
Kamal Patra <kamal.patra369@gmail.com>
2023-04-08 01:16:28 +0530

    refactored gamegrid error

--- src/components/gameGrid/GameGrid.tsx
+++ src/components/gameGrid/GameGrid.tsx
@@ -12,29 +12,27 @@ interface Props {
 const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGameList(gameQuery);
   const gameSkeletons = [1, 2, 3, 4, 5, 6, 7, 8];
-  console.log(data);
-  return (
-    <>
-      {error && <Text>{error}</Text>}
 
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        spacing={5}
-        padding="10px"
-      >
-        {isLoading &&
-          gameSkeletons.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-        {data.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} />
+  if (error) return <Text>{error}</Text>;
+
+  return (
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      spacing={5}
+      padding="10px"
+    >
+      {isLoading &&
+        gameSkeletons.map((skeleton) => (
+          <GameCardContainer key={skeleton}>
+            <GameCardSkeleton />
           </GameCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {data.map((game) => (
+        <GameCardContainer key={game.id}>
+          <GameCard game={game} />
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
