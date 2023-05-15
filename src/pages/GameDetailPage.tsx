40dd5693b117b268dd0dcc4131045fa65d73e5d2
Kamal Patra <kamal.patra369@gmail.com>
2023-05-16 02:15:32 +0530

    Built game screenshots

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -2,6 +2,7 @@ import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import ExpandableText from "../components/expandabletText/ExpandableText";
 import GameAttributes from "../components/gameAttributes/GameAttributes";
+import GameScreenshots from "../components/gameScreenshots/GameScreenshots";
 import GameTrailer from "../components/gameTrailer/GameTrailer";
 import useGameDetail from "../react-query/hooks/useGameDetail";
 
@@ -16,6 +17,7 @@ const GameDetailPage = () => {
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
       <GameTrailer gameId={game.id} />
+      <GameScreenshots gameId={game.id} />
     </>
   );
 };
