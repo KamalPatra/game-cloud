b887f17b5f439c99e4a60fad89b826f5d2cb825e
Kamal Patra <kamal.patra369@gmail.com>
2023-05-16 00:55:57 +0530

    logo link to homepage

--- src/components/navBar/Navbar.tsx
+++ src/components/navBar/Navbar.tsx
@@ -1,16 +1,15 @@
-import { Box, Button, HStack } from "@chakra-ui/react";
+import { Box, HStack } from "@chakra-ui/react";
 import { IoGameControllerOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
 import DarkModeToggle from "../darkModeToggle/DarkModeToggle";
 import SearchInput from "../searchInput/SearchInput";
 
 const Navbar = () => {
   return (
     <HStack justifyContent="space-around" p="4">
-      <Button
-        fontSize="lg"
-        leftIcon={<IoGameControllerOutline size={30} />}
-        variant="link"
-      ></Button>
+      <Link to="/">
+        <IoGameControllerOutline size={30} />
+      </Link>
       <SearchInput />
       <Box>
         <DarkModeToggle />
